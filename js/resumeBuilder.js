3f55fcd9b2fc512dd746ac1621a68209999a96bb
Jon <jone_chan@163.com>
2017-07-07 14:31:09 +0800

    make some number to be string. Reduce duplicate code. add a Url for school name.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -30,24 +30,12 @@ var bio = {
         var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
         var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
 
-        // info of me
-        $("#header").append(formattedBiopic);
-        $("#header").prepend(formattedRole);
-        $("#header").prepend(formattedName);
-
-        // welcome
-        $("#header").append(formattedWelcomeMessage);
-
+        $("#header").prepend(formattedName,formattedRole);
+        $("#header").append(formattedBiopic,formattedWelcomeMessage);
         // contacts
-        $("#topContacts").append(formattedMobile);
-        $("#topContacts").append(formattedEmail);
-        $("#topContacts").append(formattedGithub);
-        $("#topContacts").append(formattedLocation);
+        $("#topContacts").append(formattedMobile,formattedEmail,formattedGithub,formattedLocation);
         //footer contacts
-        $("#footerContacts").append(formattedMobile);
-        $("#footerContacts").append(formattedEmail);
-        $("#footerContacts").append(formattedGithub);
-        $("#footerContacts").append(formattedLocation);
+        $("#footerContacts").append(formattedMobile,formattedEmail,formattedGithub,formattedLocation);
         //Connect
         $('#lets-connect > h2').html('<a href=mailto:jone_chan@163.com>Let\'s Connect</a>');
 
@@ -72,34 +60,26 @@ var work = {
     "jobs": [{
         "title": "咖啡师",
         "employer": "星巴克",
-        "years": 4,
+        "years": "4",
         "dates": "2011/10-2015/12",
         "location": "广州",
         "description": "制作咖啡和其他饮品，运营店铺，宣传和举办店铺活动，服务顾客"
     }],
     "display": function() {
 
-        var formattedEmployer = null;
-        var formattedTitle = null;
-        var formattedEmployerTitle = null;
-        var formattedDate = null;
-        var formattedDescription = null;
-
         for (var i = 0; i < work.jobs.length; i++) {
             $("#workExperience").append(HTMLworkStart);
             // 职位和公司名
-            formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[i].employer);
-            formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[i].title);
-            formattedEmployerTitle = formattedEmployer + formattedTitle;
+            var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[i].employer);
+            var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[i].title);
+            var formattedEmployerTitle = formattedEmployer + formattedTitle;
             // 工作时间
-            formattedDate = HTMLworkDates.replace("%data%", work.jobs[i].dates);
+            var formattedDate = HTMLworkDates.replace("%data%", work.jobs[i].dates);
             // 工作内容
-            formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[i].description);
+            var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[i].description);
 
             // 插入网页
-            $(".work-entry:last").append(formattedEmployerTitle);
-            $(".work-entry:last").append(formattedDate);
-            $(".work-entry:last").append(formattedDescription);
+            $(".work-entry:last").append(formattedEmployerTitle,formattedDate,formattedDescription);
         }
     }
 };
@@ -107,45 +87,39 @@ var work = {
 work.display();
 
 //Projects
-var project = {
+var projects = {
     "projects": [{
         "title": "动物卡",
-        "dates": 2017,
+        "dates": "2017",
         "description": "这是一张介绍猎豹的卡片",
         "images": ["images/cheetah-412_small.png"]
     }, {
         "title": "My App",
-        "dates": 2017,
+        "dates": "2017",
         "description": "这是介绍My app的网页",
         "images": ["images/my_app-412_small.png"]
     }, {
         "title": "动画",
-        "dates": 2017,
+        "dates": "2017",
         "description": "蓝天白云，加上青青草原，白色的云朵会按照不同的速度从右边飘向左边",
         "images": ["images/sky_and_grass-412_small.png"]
     }],
     "display": function() {
-        var formattedTitle = null;
-        var formattedDates = null;
-        var formattedDescription = null;
-        var formattedImages = null;
 
-        for (var i = 0; i < project.projects.length; i++) {
+        for (var i = 0; i < projects.projects.length; i++) {
             $("#projects").append(HTMLprojectStart);
             // 项目名称
-            formattedTitle = HTMLprojectTitle.replace("%data%", project.projects[i].title);
+            var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[i].title);
             // 项目时间
-            formattedDates = HTMLprojectDates.replace("%data%", project.projects[i].dates);
+            var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[i].dates);
             // 项目描述
-            formattedDescription = HTMLprojectDescription.replace("%data%", project.projects[i].description);
+            var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[i].description);
             // 插入网页
-            $(".project-entry:last").append(formattedTitle);
-            $(".project-entry:last").append(formattedDates);
-            $(".project-entry:last").append(formattedDescription);
+            $(".project-entry:last").append(formattedTitle,formattedDates,formattedDescription);
             //项目图片
-            if (project.projects[i].images.length > 0) {
-                for (var j = 0; j < project.projects[i].images.length; j++) {
-                    formattedImages = HTMLprojectImage.replace("%data%", project.projects[i].images[j]);
+            if (projects.projects[i].images.length > 0) {
+                for (var j = 0; j < projects.projects[i].images.length; j++) {
+                    var formattedImages = HTMLprojectImage.replace("%data%", projects.projects[i].images[j]);
                     $(".project-entry:last").append(formattedImages);
                 }
             }
@@ -153,14 +127,14 @@ var project = {
     }
 };
 
-project.display();
+projects.display();
 
 //edu
 var education = {
     "schools": [{
         "name": "广州大学华软软件学院",
         "degree": "大专",
-        "majors": "国际经济与贸易",
+        "majors": ["国际经济与贸易"],
         "dates": "2007-2010",
         "location": "广州",
         "url": "http://www.sise.com.cn/index.html"
@@ -178,21 +152,23 @@ var education = {
         for (var i = 0; i < education.schools.length; i++) {
             $("#education").append(HTMLschoolStart);
             //学校和学位名称
-            var formattedName = HTMLschoolName.replace("%data%", education.schools[i].name);
+            var formattedName = HTMLschoolName.replace("%data%", education.schools[i].name).replace("#", education.schools[i].url);
             var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[i].degree);
             var formattedNameDegree = formattedName + formattedDegree;
             //日期
             var formattedDates = HTMLschoolDates.replace("%data%", education.schools[i].dates);
             //专业
-            var formattedMajors = HTMLschoolMajor.replace("%data%", education.schools[i].majors);
+            for (var j = 0; j < education.schools[i].majors.length; j++){
+            	var formattedMajors = HTMLschoolMajor.replace("%data%", education.schools[i].majors[j]);
+            }
             //地点
             var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[i].location);
+            //URL
+
 
             //插入网页
-            $(".education-entry:last").append(formattedNameDegree);
-            $(".education-entry:last").append(formattedDates);
-            $(".education-entry:last").append(formattedMajors);
-            $(".education-entry:last").append(formattedLocation);
+            $(".education-entry:last").append(formattedNameDegree,formattedDates,formattedMajors,formattedLocation);
+
         }
 
         //onlineCourses
@@ -208,9 +184,7 @@ var education = {
             var formattedUrl = HTMLonlineURL.replace("%data%", education.onlineCourses[i].url);
 
             //插入网页
-            $(".education-online-entry:last").append(formattedTitleSchool);
-            $(".education-online-entry:last").append(formattedDates);
-            $(".education-online-entry:last").append(formattedUrl);
+            $(".education-online-entry:last").append(formattedTitleSchool,formattedDates,formattedUrl);
         }
     }
 };
